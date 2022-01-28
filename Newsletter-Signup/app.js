dba013839bf658e3a1553dda024b8a9531da52b2
Rishabh Shekhar <rishabhshekhar04@gmail.com>
2022-01-28 17:17:10 +0530

    Added get request

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -2,7 +2,13 @@ const express = require("express");
 const request = require("request");
 const bodyparser = require("body-parser");
 
-app = express();
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname+"/signup.html");
+});
 
 app.listen(8000, function(){
     console.log("Server is running on port 8000");
