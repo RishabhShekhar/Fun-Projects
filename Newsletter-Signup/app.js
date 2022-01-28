f8dc14b86bfecf02241872ee880738fd93ea25c9
Rishabh Shekhar <rishabhshekhar04@gmail.com>
2022-01-28 14:44:49 +0530

    Deleted Newsletter

--- Newsletter-Signup/app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const request = require("request");
-const bodyparser = require("body-parser");
-
-app = express();
-
-app.listen(8000, function(){
-    console.log("Server is running on port 8000")
-})
