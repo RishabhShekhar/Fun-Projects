4c39c93d85958bbc5edd23298ea48c5aa49c3c0b
Rishabh Shekhar <rishabhshekhar04@gmail.com>
2022-01-29 21:07:41 +0530

    Completed Backend Functionalities

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -5,15 +5,34 @@ app = express();
 
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({extended:true}));
+
+var items = ["Buy Food", "Eat Food", "Cook Food"];
+
 app.get("/", function(req, res){
     const today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
-    const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday"];
-    
-    res.render("list", {kindOfDay:weekday[currentDay]});
+
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+    };
+
+    var day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", {kindOfDay:day, newitems:items});
 
 });
+
+app.post("/", function(req, res){
+
+    item = req.body.item;
+
+    items.push(item);
+
+    res.redirect("/");
+
+})
 
 app.listen(8000, function(){
     console.log("Server is running on port 8000");
