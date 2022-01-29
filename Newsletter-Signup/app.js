137fa52af92a17ecb45cc4fbc41bf4f62f3862d0
Rishabh Shekhar <rishabhshekhar04@gmail.com>
2022-01-29 11:05:17 +0530

    Final Commit

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -40,10 +40,17 @@ app.post("/", function(req, res){
 
     const options = {
         method: "POST",
-        auth: "bob:acbdaf3de76719ef91ca2133444f20d0-us2"
+        auth: "bob:f7e1563f66d010f0d87140af77df182-us2"
     };
 
     const request = https.request(url, options, function(response){
+
+        if (response.statusCode == 200){
+            res.sendFile(__dirname+"/success.html");
+        }
+        else{
+            res.sendFile(__dirname+"/failure.html");
+        }
         response.on("data", function(data){
             console.log(JSON.parse(data));
         })
